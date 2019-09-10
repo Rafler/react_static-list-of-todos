2b67c35a7510756330e0a6dfd91c400d7c40868f
Rafler <amuronyk@gmail.com>
2019-09-10 20:22:27 +0300

    Updates

---
+++ precache-manifest.32353a96942619aefa89b935993caa75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60212f5c1ddbfbee026cc1fcb73538a",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "79adebaffdc3bed35610",
+    "url": "/react_static-list-of-todos/static/css/main.fbbe2ac3.chunk.css"
+  },
+  {
+    "revision": "fa67027c564020748778",
+    "url": "/react_static-list-of-todos/static/js/2.78963138.chunk.js"
+  },
+  {
+    "revision": "79adebaffdc3bed35610",
+    "url": "/react_static-list-of-todos/static/js/main.057d41fb.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  }
+]);
